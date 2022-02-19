26491079936bba20034e8d86d843293734ba9381
Caio <kayoroxa@gmail.com>
2022-02-19 12:41:05 -0300

    valor plano anual

--- src/components/Valor.tsx
+++ src/components/Valor.tsx
@@ -11,9 +11,14 @@ export const Container = styled.div<SProps>`
   display: flex;
   position: relative;
   justify-content: center;
+  .price {
+    width: 80%;
+  }
   .price-wrapper {
     display: flex;
     position: relative;
+    flex-direction: column;
+    justify-content: center;
   }
 
   font-size: 20px;
@@ -21,6 +26,11 @@ export const Container = styled.div<SProps>`
     color: ${({ notColor }) => !notColor && 'orange'};
     display: flex;
     flex-direction: column;
+    text-align: center;
+    margin-top: 15px;
+    * {
+      line-height: 16px;
+    }
   }
   .right {
     margin-left: 10px;
@@ -33,8 +43,15 @@ export const Container = styled.div<SProps>`
       bottom: 0;
     }
   }
+  .oldValue {
+    text-align: center;
+    margin-bottom: 10px
+    font-weight: lighter;
+    color: #bbbbbb;
+    font-size: 1rem;
+  }
 
-  .avista {
+  .bottom {
     color: ${({ notColor }) => !notColor && 'orange'};
     text-align: center;
     width: 100%;
@@ -51,22 +68,32 @@ export default function Valor({ removeOldValue, notColor }: Props) {
   return (
     <Container notColor={notColor}>
       <div className="price">
-        {!removeOldValue && (
-          <b style={{ textDecoration: 'line-through' }}>De R$997</b>
-        )}
         <div className="price-wrapper">
+          {!removeOldValue && (
+            <div className="oldValue">
+              <span>De </span>
+              <span style={{ textDecoration: 'line-through' }}>
+                <>R$ 58 / mês</>
+              </span>
+              <span> por</span>
+            </div>
+          )}
           <div className="left">
-            <b>Por apenas</b>
-            <b style={{ fontSize: '25px' }}>12x de R$</b>
-          </div>
-          <div className="right">
             <b>
-              31,<span style={{ fontSize: '40px' }}>86</span>
+              <span style={{ fontSize: '1.5rem' }}>R$ </span>
+              <span style={{ fontSize: '4rem' }}>31</span>
+              <span style={{ fontSize: '1.5rem' }}>/mês</span>
+              {/* 31,<span style={{ fontSize: '40px' }}>86</span> */}
             </b>
           </div>
+          <div className="right">
+            {/* <b>Por apenas</b> */}
+            {/* <b style={{ fontSize: '25px' }}>12x de R$</b> */}
+          </div>
         </div>
-        <p className="avista">
-          Ou <strong>R$327,00</strong> a vista.
+        <p className="bottom">
+          Plano anual
+          {/* Ou <strong>R$327,00</strong> a vista. */}
         </p>
       </div>
     </Container>
