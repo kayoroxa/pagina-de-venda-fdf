8bba21c4a2ff6f1be8ad4dd132a59c4e91d7949f
Caio <kayoroxa@gmail.com>
2022-03-21 14:58:36 -0300

    check if exist key db

--- src/pages/api/user/[number].js
+++ src/pages/api/user/[number].js
@@ -7,22 +7,11 @@ export default async function handler(req, res) {
   } = req
 
   switch (method) {
-    // case 'GET' /* Get a model by its ID */:
-    //   // try {
-    //   //   const users = await axios.get(
-    //   //     `https://api.jsonstorage.net/v1/json/05e182a5-66dd-4dfb-b45f-8fd701ea413d/1ed36400-4397-4f37-831b-65ef1cffa821`
-    //   //   )
-    //   //   if (!users) {
-    //   //     return res.status(400).json({ success: false })
-    //   //   }
-    //   res.status(200).json({ success: true, number })
-    //   // } catch (error) {
-    //   //   res.status(400).json({ success: false })
-    //   // }
-
-    //   break
-
     case 'PUT' /* Edit a model by its ID */:
+      // console.log(process.env.jsonstorage_key)
+      if (!process.env.jsonstorage_key) {
+        return res.status(400).json({ success: false, data: 'No DB KEY' })
+      }
       try {
         const users = await axios
           .get(
