aec99415a177b26681dfe7588671b5267e181178
Caio <kayoroxa@gmail.com>
2022-03-21 14:58:49 -0300

    get test fix

--- src/pages/api/user/index.js
+++ src/pages/api/user/index.js
@@ -21,37 +21,5 @@ export default async function handler(req, res) {
       // }
 
       break
-
-    case 'PUT' /* Edit a model by its ID */:
-      try {
-        const users = await axios
-          .get(
-            `https://api.jsonstorage.net/v1/json/05e182a5-66dd-4dfb-b45f-8fd701ea413d/1ed36400-4397-4f37-831b-65ef1cffa821`
-          )
-          .then(res => res.data)
-
-        if (!users) {
-          return res
-            .status(400)
-            .json({ success: false, data: "Don't have data" })
-        } else if (!Array.isArray(users)) {
-          return res
-            .status(400)
-            .json({ success: false, data: 'Is not Array', users })
-        }
-
-        const user = await axios.put(
-          `https://api.jsonstorage.net/v1/json/05e182a5-66dd-4dfb-b45f-8fd701ea413d/1ed36400-4397-4f37-831b-65ef1cffa821?apiKey=${process.env.jsonstorage_key}`,
-          [...users, number]
-        )
-
-        if (!user) {
-          return res.status(400).json({ success: false })
-        }
-        res.status(200).json({ success: true })
-      } catch (error) {
-        res.status(400).json({ success: false, error: error.message })
-      }
-      break
   }
 }
