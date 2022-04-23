948cb1df06ca51d627d32673603d69f6f95c4742
Caio <kayoroxa@gmail.com>
2022-04-23 14:22:13 -0300

    fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,5 @@
-import { makeStyles } from '@material-ui/core'
 import { useRouter } from 'next/router'
 import PageOnlyButton from '../template/PageOnlyButton'
-
-// const colorButton = '#b3b3b3'
-const colorButton = '#00bb3e'
-const buttonTitle = 'Se cadastrar agora!'
-
-const useStyles = makeStyles({
-  flexGrow: {
-    flex: '1',
-  },
-  paper: {
-    rounded: true,
-  },
-  button: {
-    backgroundColor: '#fff',
-    color: colorButton,
-    borderColor: colorButton,
-    // fontSize: 'min(5vw, 20px)',
-    fontSize: 'min(4vw, 17px)',
-    borderRadius: '40px',
-    width: 'min(90vw, 600px)',
-    height: 'max(8vh, 70px)',
-    '&:hover': {
-      backgroundColor: colorButton,
-      color: '#fff',
-    },
-  },
-  buttonFill: {
-    backgroundColor: colorButton,
-    color: '#fff',
-    borderColor: colorButton,
-    fontSize: 'min(4vw, 17px)',
-    borderRadius: '40px',
-    width: 'min(90vw, 500px)',
-    height: 'max(8vh, 70px)',
-    '&:hover': {
-      backgroundColor: colorButton,
-      color: '#fff',
-    },
-  },
-})
 
 export default function Home() {
   const router = useRouter()
