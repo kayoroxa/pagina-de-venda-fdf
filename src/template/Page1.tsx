9b2ea6326db8a8d796e6619d5413ce5fea4f8293
Caio <kayoroxa@gmail.com>
2023-01-31 17:47:39 -0300

    feature artur link src

--- src/template/Page1.tsx
+++ src/template/Page1.tsx
@@ -86,6 +86,13 @@ export default function Page1({
       ? 'https://forms.gle/ySuoH6zZrBw2H15S6'
       : 'https://bit.ly/34PctC3'
 
+  const isArtur =
+    mySrc && typeof mySrc === 'string' && mySrc.toLowerCase().endsWith('-a')
+
+  if (isArtur) {
+    linkCheckOut = 'https://go.hotmart.com/E53339888M?ap=d4f3&split=12'
+  }
+
   if (mySrc) {
     linkCheckOut += `&src=${mySrc}`
   }
