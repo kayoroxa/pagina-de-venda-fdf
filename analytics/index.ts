054ab93b5fed81b0f28eb293ea8f58d86cd106ba
your Name <kayoroxa@gmail.com>
2023-11-04 23:56:33 -0300

    447 price

--- analytics/index.ts
+++ analytics/index.ts
@@ -40,41 +40,59 @@ let variables = {
   ],
   checkoutAndPrice: [
     {
-      tag: '397',
+      tag: '497',
       showPercent: 1,
       data: {
-        value: 397,
-        link: 'https://pay.hotmart.com/Y51115808H?off=fzv3lnkr&checkoutMode=10&split=12',
+        value: 497,
+        link: 'https://pay.hotmart.com/Y51115808H?off=c74kxq7h&checkoutMode=10&split=12',
       },
     },
+    // {
+    //   tag: '397',
+    //   showPercent: 1,
+    //   data: {
+    //     value: 397,
+    //     link: 'https://pay.hotmart.com/Y51115808H?off=fzv3lnkr&checkoutMode=10&split=12',
+    //   },
+    // },
   ],
   video: [
-    {
-      tag: 'omega10',
-      showPercent: 0.1,
-      data: {
-        value: 397,
-        youtubeVideoId: 'Sos6wA7qKD0',
-        time: '04:04', //244  2.32%
-      },
-    },
-    {
-      tag: 'omega',
-      showPercent: 0.1,
-      data: {
-        value: 397,
-        youtubeVideoId: '4G-DAgCicDs',
-        time: '05:00', //300  1.86%
-      },
-    },
+    // {
+    //   tag: 'omega10',
+    //   showPercent: 0.1,
+    //   data: {
+    //     value: 397,
+    //     youtubeVideoId: 'Sos6wA7qKD0',
+    //     time: '04:04', //244  2.32%
+    //   },
+    // },
+    // {
+    //   tag: 'omega',
+    //   showPercent: 0.1,
+    //   data: {
+    //     value: 397,
+    //     youtubeVideoId: '4G-DAgCicDs',
+    //     time: '05:00', //300  1.86%
+    //   },
+    // },
+
+    // {
+    //   tag: 'omega',
+    //   showPercent: 0.8,
+    //   data: {
+    //     value: 397,
+    //     youtubeVideoId: '4G-DAgCicDs',
+    //     time: '08:45', //525   1.66%
+    //   },
+    // },
 
     {
-      tag: 'omega',
-      showPercent: 0.8,
+      tag: 'alf',
+      showPercent: 1,
       data: {
-        value: 397,
-        youtubeVideoId: '4G-DAgCicDs',
-        time: '08:45', //525   1.66%
+        value: 447,
+        youtubeVideoId: 'rjoE2kAwpn0',
+        time: '08:54', //525   1.66%
       },
     },
 
